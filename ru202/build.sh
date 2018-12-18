aae883412caaca6602c6dabe7acf10732441c1e4
Alvin Richards <alvin.richards@mac.com>
2018-12-18 13:00:23 -0800

    Initial checkin for RU202 labs

---
+++ ru202/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export _REDIS_VER="5.0.2"
+
+
+if [ -n "$1" ]
+then
+  	export _REDIS_VER=$1
+fi
+
+
+sed 's/$REDIS_VERSION/'$_REDIS_VER'/' Dockerfile.template > Dockerfile.$_REDIS_VER
+echo Building $_REDIS_VER
+
+docker build -f Dockerfile.$_REDIS_VER -t redisuniversity/ru202-lab .
