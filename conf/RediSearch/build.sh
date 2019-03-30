804f9cc787487374643190de330033e40fdfef66
Alvin Richards <alvin.richards@mac.com>
2019-03-30 07:57:01 -0700

    EDU-216 next pass of chnages for RedisConf

--- conf/RediSearch/build.sh
+++ conf/RediSearch/build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker build -t redisuniversity/redisconf-redisearch .
+export _REDIS_VER="5.0.3"
+
+
+if [ -n "$1" ]
+then
+  	export _REDIS_VER=$1
+fi
+
+
+sed 's/$REDIS_VERSION/'$_REDIS_VER'/' Dockerfile.template > Dockerfile.$_REDIS_VER
+echo Building $_REDIS_VER
+
+docker build -f Dockerfile.$_REDIS_VER -t redisuniversity/redisconf-redisearch .
