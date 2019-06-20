65a36e810f2e97bf7fbe531dcfd7d07e138580f2
Simon Prickett <simon@redislabs.com>
2019-06-19 17:36:54 -0700

    Addressed linting issues.

--- ru102js/redisolar/util/dataloader.js
+++ ru102js/redisolar/util/dataloader.js
@@ -1,24 +1,25 @@
 const path = require('path');
 
-const loadData = filename => {
+const loadData = (filename) => {
+  /* eslint-disable global-require, import/no-dynamic-require */
   const sampleData = require(filename);
 
   for (const site of sampleData) {
     console.log(site);
   }
 
-  console.log(`Loaded ${sampleData.length} sites.`)
-} 
+  console.log(`Loaded ${sampleData.length} sites.`);
+};
 
 if (process.argv.length !== 4) {
-  console.error('Usage: npm run load <path_to_json_data_file>')
+  console.error('Usage: npm run load <path_to_json_data_file>');
 } else {
   const filename = process.argv[3];
 
   try {
     loadData(path.resolve(__dirname, '../', filename));
   } catch (e) {
-    console.error(`Error loading ${filename}:`)
-    console.error(e)
+    console.error(`Error loading ${filename}:`);
+    console.error(e);
   }
 }
