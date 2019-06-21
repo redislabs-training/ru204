6b86fa628cdc734de11f0ac54ceb55f3e3784ea1
Kyle Banker <kyle.banker@redislabs.com>
2019-06-21 14:41:01 -0600

    Remove legacy redisolar

--- ru102j/redisolar/src/main/java/com/redislabs/university/RU102J/dao/CapacityDao.java
+++
@@ -1,9 +0,0 @@
-package com.redislabs.university.RU102J.dao;
-
-import com.redislabs.university.RU102J.api.CapacityReport;
-import com.redislabs.university.RU102J.api.MeterReading;
-
-public interface CapacityDao {
-    void update(MeterReading reading);
-    CapacityReport getReport(Integer limit);
-}
