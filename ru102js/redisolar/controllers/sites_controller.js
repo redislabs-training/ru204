d49d9c02266f43c8ffff75a9a5aa680440b99a3a
Simon Prickett <simon@redislabs.com>
2019-06-26 11:36:37 -0700

    Moved response objects out of controller into static DAO, set default DB to static.

--- ru102js/redisolar/controllers/sites_controller.js
+++ ru102js/redisolar/controllers/sites_controller.js
@@ -1,50 +1,11 @@
-const getSites = (req, res) => res.status(200).json([{
-  id: 149,
-  capacity: 9.0,
-  panels: 6,
-  address: '856 Milton Street ',
-  city: 'Oakland',
-  state: 'CA',
-  postalCode: '94607',
-  coordinate: {
-    lng: -122.276749,
-    lat: 37.817176,
-  },
-  lastReportingTime: 1560967226.262000000,
-  meterReadingCount: 180,
-}, {
-  id: 31,
-  capacity: 7.5,
-  panels: 5,
-  address: '894 Windmill Park Lane ',
-  city: 'Mountain View',
-  state: 'CA',
-  postalCode: '94043',
-  coordinate: {
-    lng: -122.077478,
-    lat: 37.399505,
-  },
-  lastReportingTime: 1560967216.141000000,
-  meterReadingCount: 180,
-}]);
+const siteDao = require('../daos/site_dao');
+
+const getSites = (req, res) => res.status(200).json(siteDao.findAll());
 
 const getSite = (req, res) => {
   const { siteId } = req.params;
-  return res.status(200).json([{
-    id: siteId,
-    capacity: 7.5,
-    panels: 5,
-    address: '894 Windmill Park Lane ',
-    city: 'Mountain View',
-    state: 'CA',
-    postalCode: '94043',
-    coordinate: {
-      lng: -122.077478,
-      lat: 37.399505,
-    },
-    lastReportingTime: 1560967216.141000000,
-    meterReadingCount: 180,
-  }]);
+
+  return res.status(200).json(siteDao.findById(siteId));
 };
 
 const getSitesNearby = (req, res) => {
@@ -60,36 +21,8 @@ const getSitesNearby = (req, res) => {
   console.log(`radiusUnit: ${radiusUnit}`);
   console.log(`onlyExcessCapacity: ${onlyExcessCapacity}`);
 
-  return res.status(200).json([{
-    id: 149,
-    capacity: 9.0,
-    panels: 6,
-    address: '856 Milton Street ',
-    city: 'Oakland',
-    state: 'CA',
-    postalCode: '94607',
-    coordinate: {
-      lng: -122.276749,
-      lat: 37.817176,
-    },
-    lastReportingTime: 1560967226.262000000,
-    meterReadingCount: 180,
-  }, {
-    id: 31,
-    capacity: 7.5,
-    panels: 5,
-    address: '894 Windmill Park Lane ',
-    city: 'Mountain View',
-    state: 'CA',
-    postalCode: '94043',
-    coordinate: {
-      lng: -122.077478,
-      lat: 37.399505,
-    },
-    lastReportingTime: 1560967216.141000000,
-    meterReadingCount: 180,
-  }]);
-};
+  return res.status(200).json(siteDao.findByGeo('TODO'));
+}
 
 module.exports = {
   getSites,
--- ru102js/redisolar/daos/impl/static/site_dao_static_impl.js
+++ ru102js/redisolar/daos/impl/static/site_dao_static_impl.js
@@ -1,10 +1,80 @@
 const insert = site => 'Static TODO';
 
-const findById = id => 'Static TODO';
+const findById = id => [{
+  id,
+  capacity: 7.5,
+  panels: 5,
+  address: '894 Windmill Park Lane ',
+  city: 'Mountain View',
+  state: 'CA',
+  postalCode: '94043',
+  coordinate: {
+    lng: -122.077478,
+    lat: 37.399505,
+  },
+  lastReportingTime: 1560967216.141000000,
+  meterReadingCount: 180,
+}];
 
-const findAll = () => 'Static TODO';
+const findAll = () => [{
+  id: 149,
+  capacity: 9.0,
+  panels: 6,
+  address: '856 Milton Street ',
+  city: 'Oakland',
+  state: 'CA',
+  postalCode: '94607',
+  coordinate: {
+    lng: -122.276749,
+    lat: 37.817176,
+  },
+  lastReportingTime: 1560967226.262000000,
+  meterReadingCount: 180,
+}, {
+  id: 31,
+  capacity: 7.5,
+  panels: 5,
+  address: '894 Windmill Park Lane ',
+  city: 'Mountain View',
+  state: 'CA',
+  postalCode: '94043',
+  coordinate: {
+    lng: -122.077478,
+    lat: 37.399505,
+  },
+  lastReportingTime: 1560967216.141000000,
+  meterReadingCount: 180,
+}];
 
-const findByGeo = query => 'Static TODO';
+const findByGeo = query => [{
+  id: 149,
+  capacity: 9.0,
+  panels: 6,
+  address: '856 Milton Street ',
+  city: 'Oakland',
+  state: 'CA',
+  postalCode: '94607',
+  coordinate: {
+    lng: -122.276749,
+    lat: 37.817176,
+  },
+  lastReportingTime: 1560967226.262000000,
+  meterReadingCount: 180,
+}, {
+  id: 31,
+  capacity: 7.5,
+  panels: 5,
+  address: '894 Windmill Park Lane ',
+  city: 'Mountain View',
+  state: 'CA',
+  postalCode: '94043',
+  coordinate: {
+    lng: -122.077478,
+    lat: 37.399505,
+  },
+  lastReportingTime: 1560967216.141000000,
+  meterReadingCount: 180,
+}];
 
 module.exports = {
   insert,
