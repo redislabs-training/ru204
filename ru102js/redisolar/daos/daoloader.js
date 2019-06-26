aba13d11ce4ee6c68a0a22bc3d19b06dea6a5a47
Simon Prickett <simon@redislabs.com>
2019-06-25 17:47:34 -0700

    Fixed eslint comment.

--- ru102js/redisolar/daos/daoloader.js
+++ ru102js/redisolar/daos/daoloader.js
@@ -1,6 +1,6 @@
 const config = require('better-config');
 
-/* eslint-disable import/no-dynamic-require global-require */
+/* eslint-disable import/no-dynamic-require, global-require */
 const loadDao = daoName => require(`./impl/${config.get('application.dataStore')}/${daoName}_dao_${config.get('application.dataStore')}_impl`);
 /* eslint-enable */
 
