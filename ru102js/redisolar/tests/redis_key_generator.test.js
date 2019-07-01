b7352a88faf6a8c4ba3f758c4ecb455ecab12c40
Simon Prickett <simon@redislabs.com>
2019-07-01 14:28:09 -0700

    Adds use of Jest todo tests.

--- ru102js/redisolar/tests/redis_key_generator.test.js
+++ ru102js/redisolar/tests/redis_key_generator.test.js
@@ -3,47 +3,44 @@ const keyGenerator = require('../src/daos/impl/redis/redis_key_generator');
 
 config.set('../config.json');
 
+const testSuiteName = 'redis_key_generator';
 const expectedKeyPrefix = config.get('dataStores.redis.keyPrefix');
 
 /* eslint-disable no-undef */
 
-test('Key Generator:getSiteHashId', () => {
+test(`${testSuiteName}:getSiteHashId`, () => {
   expect(keyGenerator.getSiteHashKey(999)).toBe(`${expectedKeyPrefix}:sites:info:999`);
 });
 
-test('Key Generator:getSiteIDsKey', () => {
+test(`${testSuiteName}:getSiteIDsKey`, () => {
   expect(keyGenerator.getSiteIDsKey()).toBe(`${expectedKeyPrefix}:sites:ids`);
 });
 
-test('Key Generator:getSiteStatsKey', () => {
-  expect(keyGenerator.getSiteStatsKey()).toBe('TODO'); // TODO real test...
-});
+test(`${testSuiteName}:getSiteStatsKey`, test.todo('Test needed.'));
 
-test('Key Generator:getRateLimiterKey', () => {
+test(`${testSuiteName}:getRateLimiterKey`, () => {
   expect(keyGenerator.getRateLimiterKey('test', 1, 12)).toBe(`${expectedKeyPrefix}:test:1:12`);
 });
 
-test('Key Generator:getSiteGeoKey', () => {
+test(`${testSuiteName}:getSiteGeoKey`, () => {
   expect(keyGenerator.getSiteGeoKey()).toBe(`${expectedKeyPrefix}:sites:geo`);
 });
 
-test('Key Generator:getCapacityRankingKey', () => {
+test(`${testSuiteName}:getCapacityRankingKey`, () => {
   expect(keyGenerator.getCapacityRankingKey()).toBe(`${expectedKeyPrefix}:sites:capacity:ranking`);
 });
 
-test('Key Generator:getTSKey', () => {
+test(`${testSuiteName}:getTSKey`, () => {
   expect(keyGenerator.getTSKey(99, 'test')).toBe(`${expectedKeyPrefix}:sites:ts:99:test`);
 });
 
-test('Key Generator:getDayMetricKey', () => {
-  expect(keyGenerator.getDayMetricKey(99, 'TODO', 0)).toBe('TODO'); // TODO real test...
-});
+test(`${testSuiteName}:getDayMetricKey`, test.todo('Test needed.'));
 
-test('Key Generator:getGlobalFeedKey', () => {
+test(`${testSuiteName}:getGlobalFeedKey`, () => {
   expect(keyGenerator.getGlobalFeedKey()).toBe(`${expectedKeyPrefix}:sites:feed`);
 });
 
-test('Key Generator:getFeedKey', () => {
+test(`${testSuiteName}:getFeedKey`, () => {
   expect(keyGenerator.getFeedKey(99)).toBe(`${expectedKeyPrefix}:sites:feed:99`);
 });
 
