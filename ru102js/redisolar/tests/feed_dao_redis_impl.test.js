224b0bab3d2c6b731f56038c564fd994bd1e59ce
Simon Prickett <simon@redislabs.com>
2019-07-02 10:48:27 -0700

    Updates tests to include correct DAOs.

---
+++ ru102js/redisolar/tests/feed_dao_redis_impl.test.js
@@ -0,0 +1,31 @@
+const config = require('better-config');
+const redis = require('../src/daos/impl/redis/redis_client');
+const redisFeedDAO = require('../src/daos/impl/redis/feed_dao_redis_impl');
+const keyGenerator = require('../src/daos/impl/redis/redis_key_generator');
+
+const testSuiteName = 'site_dao_redis_impl';
+
+const testKeyPrefix = 'test';
+
+config.set('../config.json');
+keyGenerator.setPrefix('test');
+const client = redis.getClient();
+
+/* eslint-disable no-undef */
+
+afterEach(async () => {
+  const testKeys = await client.keysAsync(`${testKeyPrefix}:*`);
+
+  if (testKeys.length > 0) {
+    await client.delAsync(testKeys);
+  }
+});
+
+afterAll(() => {
+  // Release Redis connection.
+  client.quit();
+});
+
+test.todo(`${testSuiteName}: write tests!`);
+
+/* eslint-enable */
