96e8328c58d9a187285a9801ce64469ac14a16b7
Simon Prickett <simon@redislabs.com>
2019-07-02 10:27:43 -0700

    Standardized test names.

--- ru102js/redisolar/tests/redis_key_generator.test.js
+++ ru102js/redisolar/tests/redis_key_generator.test.js
@@ -11,43 +11,43 @@ keyGenerator.setPrefix(expectedKeyPrefix);
 
 /* eslint-disable no-undef */
 
-test(`${testSuiteName}:getSiteHashId`, () => {
+test(`${testSuiteName}: getSiteHashId`, () => {
   expect(keyGenerator.getSiteHashKey(999)).toBe(`${expectedKeyPrefix}:sites:info:999`);
 });
 
-test(`${testSuiteName}:getSiteIDsKey`, () => {
+test(`${testSuiteName}: getSiteIDsKey`, () => {
   expect(keyGenerator.getSiteIDsKey()).toBe(`${expectedKeyPrefix}:sites:ids`);
 });
 
-test.todo(`${testSuiteName}:getSiteStatsKey`);
+test.todo(`${testSuiteName}: getSiteStatsKey`);
 
-test(`${testSuiteName}:getRateLimiterKey`, () => {
+test(`${testSuiteName}: getRateLimiterKey`, () => {
   expect(keyGenerator.getRateLimiterKey('test', 1, 12)).toBe(`${expectedKeyPrefix}:test:1:12`);
 });
 
-test(`${testSuiteName}:getSiteGeoKey`, () => {
+test(`${testSuiteName}: getSiteGeoKey`, () => {
   expect(keyGenerator.getSiteGeoKey()).toBe(`${expectedKeyPrefix}:sites:geo`);
 });
 
-test(`${testSuiteName}:getCapacityRankingKey`, () => {
+test(`${testSuiteName}: getCapacityRankingKey`, () => {
   expect(keyGenerator.getCapacityRankingKey()).toBe(`${expectedKeyPrefix}:sites:capacity:ranking`);
 });
 
-test(`${testSuiteName}:getTSKey`, () => {
+test(`${testSuiteName}: getTSKey`, () => {
   expect(keyGenerator.getTSKey(99, 'test')).toBe(`${expectedKeyPrefix}:sites:ts:99:test`);
 });
 
-test.todo(`${testSuiteName}:getDayMetricKey`);
+test.todo(`${testSuiteName}: getDayMetricKey`);
 
-test(`${testSuiteName}:getGlobalFeedKey`, () => {
+test(`${testSuiteName}: getGlobalFeedKey`, () => {
   expect(keyGenerator.getGlobalFeedKey()).toBe(`${expectedKeyPrefix}:sites:feed`);
 });
 
-test(`${testSuiteName}:getFeedKey`, () => {
+test(`${testSuiteName}: getFeedKey`, () => {
   expect(keyGenerator.getFeedKey(99)).toBe(`${expectedKeyPrefix}:sites:feed:99`);
 });
 
-test(`${testSuiteName}:setPrefix`, () => {
+test(`${testSuiteName}: setPrefix`, () => {
   expect(keyGenerator.getSiteIDsKey()).toBe(`${expectedKeyPrefix}:sites:ids`);
 });
 
