07dfd6057ba618123c66b8680ff1b26e1dd3035c
Simon Prickett <simon@redislabs.com>
2019-07-05 20:01:58 -0700

    First go at implementing metrics.

--- ru102js/redisolar/src/daos/impl/redis/metric_dao_redis_impl.js
+++ ru102js/redisolar/src/daos/impl/redis/metric_dao_redis_impl.js
@@ -1,6 +1,6 @@
 const insert = meterReading => 'Redis TODO';
 
-const getRecent = (siteId, unit, time, limit) => 'Redis TODO';
+const getRecent = (siteId, unit, time, limit) => `Redis TODO ${unit}`;
 
 module.exports = {
   insert,
