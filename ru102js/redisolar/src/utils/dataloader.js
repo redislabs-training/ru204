a40c87d5a059378f14a63ba3fcb51d2dbd683e42
Simon Prickett <simon@redislabs.com>
2019-07-08 09:58:16 -0700

    Loads the site data now.

--- ru102js/redisolar/src/utils/dataloader.js
+++ ru102js/redisolar/src/utils/dataloader.js
@@ -5,43 +5,50 @@ const redis = require('../daos/impl/redis/redis_client');
 config.set('../../config.json');
 
 const client = redis.getClient();
+const siteDAO = require('../daos/site_dao');
 
-const flushDB = () => {
-  // TODO flush DB...
-};
+const flushDB = async () => client.flushdbAsync();
 
-const loadData = (filename, flushDb) => {
+const loadData = async (filename, flushDb) => {
   /* eslint-disable global-require, import/no-dynamic-require */
   const sampleData = require(filename);
+  /* eslint-enable */
 
   if (sampleData && flushDb) {
     console.log('Flushing database before loading sample data.');
-    flushDB();
+    await flushDB();
   }
 
-  for (const site of sampleData) {
-   // console.log(site);
-  }
+  await Promise.all(sampleData.map((site) => siteDAO.insert(site)));
+
+  // for (const site of sampleData) {
+  //   siteDAO.insert(site);
+  //   console.log(site);
+  // }
 
   console.log(`Loaded ${sampleData.length} sites.`);
 };
 
-if (process.argv.length !== 4 && process.argv.length !== 5) {
-  console.error('Usage: npm run load <path_to_json_data_file> [flushdb]');
-} else {
-  const filename = process.argv[3];
-  let flushDb = false;
+const runDataLoader = async (params) => {
+  if (params.length !== 4 && params.length !== 5) {
+    console.error('Usage: npm run load <path_to_json_data_file> [flushdb]');
+  } else {
+    const filename = params[3];
+    let flushDb = false;
 
-  if (process.argv.length === 5 && process.argv[4] === 'flushdb') {
-    flushDb = true;
+    if (params.length === 5 && params[4] === 'flushdb') {
+      flushDb = true;
+    }
+
+    try {
+      await loadData(path.resolve(__dirname, '../../', filename), flushDb);
+    } catch (e) {
+      console.error(`Error loading ${filename}:`);
+      console.error(e);
+    }
+
+    client.quit();
   }
+};
 
-  try {
-    loadData(path.resolve(__dirname, '../../', filename), flushDb);
-  } catch (e) {
-    console.error(`Error loading ${filename}:`);
-    console.error(e);
-  }
-
-  client.quit();
-}
+runDataLoader(process.argv);
