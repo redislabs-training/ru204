ae222f28603803ebfb8b79544ac16b0686be8cbd
Simon Prickett <simon@redislabs.com>
2019-07-08 14:50:46 -0700

    Refactored capacity controller for reuse and testing.

--- ru102js/redisolar/src/controllers/capacity_controller.js
+++ ru102js/redisolar/src/controllers/capacity_controller.js
@@ -1,19 +1,6 @@
-const logger = require('../utils/logger');
 const capacityDao = require('../daos/capacity_dao');
 
-const getCapacityReport = async (req, res, next) => {
-  let capacityReport;
-
-  try {
-    const { limit } = req.query;
-    logger.debug(`Limit = ${limit}`);
-    capacityReport = await capacityDao.getReport(limit);
-  } catch (err) {
-    return next(err);
-  }
-
-  return res.status(200).json(capacityReport);
-};
+const getCapacityReport = async limit => capacityDao.getReport(limit);
 
 module.exports = {
   getCapacityReport,
--- ru102js/redisolar/src/routes/capacity_routes.js
+++ ru102js/redisolar/src/routes/capacity_routes.js
@@ -3,13 +3,23 @@ const { query } = require('express-validator');
 const apiErrorReporter = require('../utils/apierrorreporter');
 const controller = require('../controllers/capacity_controller');
 
+const getLimit = n => (Number.isNaN(n) ? 10 : n);
+
 router.get(
   '/capacity',
   [
     query('limit').optional().isInt({ min: 1 }).toInt(),
     apiErrorReporter,
   ],
-  controller.getCapacityReport,
+  async (req, res, next) => {
+    try {
+      const capacityReport = await controller.getCapacityReport(getLimit(req.query.limit));
+
+      return res.status(200).json(capacityReport);
+    } catch (err) {
+      return next(err);
+    }
+  },
 );
 
 module.exports = router;
