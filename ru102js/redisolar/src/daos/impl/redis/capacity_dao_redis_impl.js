35f18b2a2464495332955adec3d5d3807849de96
Simon Prickett <simon@redislabs.com>
2019-07-08 22:04:36 -0700

    Adds working capacity report code for Redis.

--- ru102js/redisolar/src/daos/impl/redis/capacity_dao_redis_impl.js
+++ ru102js/redisolar/src/daos/impl/redis/capacity_dao_redis_impl.js
@@ -1,20 +1,47 @@
 const redis = require('./redis_client');
 const keyGenerator = require('./redis_key_generator');
 
+const remap = (arr) => {
+  const remapped = [];
+
+  for (let n = 0; n < arr.length; n += 2) {
+    remapped.push({
+      siteId: arr[n],
+      capacity: parseFloat(arr[n + 1]),
+    });
+  }
+
+  return remapped;
+};
+
 const update = async (meterReading) => {
   const client = redis.getClient();
   const currentCapacity = meterReading.whGenerated - meterReading.whUsed;
 
-  await client.zaddAsync([
+  await client.zaddAsync(
     keyGenerator.getCapacityRankingKey(),
     currentCapacity,
     meterReading.siteId,
-  ]);
+  );
 };
 
-const getReport = limit => 'Redis TODO';
+const getReport = async (limit) => {
+  const client = redis.getClient();
+  const capacityRankingKey = keyGenerator.getCapacityRankingKey();
+  const pipeline = client.batch();
 
-const getRank = siteId => 'Redis TODO';
+  pipeline.zrange(capacityRankingKey, 0, limit - 1, 'WITHSCORES');
+  pipeline.zrevrange(capacityRankingKey, 0, limit - 1, 'WITHSCORES');
+
+  const results = await pipeline.execAsync();
+
+  return {
+    lowestCapacity: remap(results[0]),
+    highestCapacity: remap(results[1]),
+  };
+};
+
+const getRank = async siteId => 'Redis TODO';
 
 module.exports = {
   update,
