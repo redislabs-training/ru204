341d830a41301602fd99ed47645c7360ddd1fcbc
Simon Prickett <simon@redislabs.com>
2019-07-08 21:24:56 -0700

    Implements updating of capacity sorted set.

--- ru102js/redisolar/src/controllers/meterreadings_controller.js
+++ ru102js/redisolar/src/controllers/meterreadings_controller.js
@@ -4,15 +4,14 @@ const capacityDao = require('../daos/capacity_dao');
 const feedDao = require('../daos/feed_dao');
 
 const createMeterReadings = async (meterReadings) => {
-  // TODO does order matter here, or are there
-  // dependencies... if so a for loop with multiple
-  // awaits might be better...
-  await meterReadings.map(async meterReading => Promise.all([
-    metricDao.insert(meterReading),
-    siteStatsDao.update(meterReading),
-    capacityDao.update(meterReading),
-    feedDao.insert(meterReading),
-  ]));
+  for (const meterReading of meterReadings) {
+    /* eslint-disable no-await-in-loop */
+    await metricDao.insert(meterReading);
+    await siteStatsDao.update(meterReading);
+    await capacityDao.update(meterReading);
+    await feedDao.insert(meterReading);
+    /* eslint-enable */
+  }
 };
 
 const getMeterReadings = async limit => feedDao.getRecentGlobal(limit);
--- ru102js/redisolar/src/daos/impl/redis/capacity_dao_redis_impl.js
+++ ru102js/redisolar/src/daos/impl/redis/capacity_dao_redis_impl.js
@@ -1,4 +1,16 @@
-const update = meterReading => 'Redis TODO';
+const redis = require('./redis_client');
+const keyGenerator = require('./redis_key_generator');
+
+const update = async (meterReading) => {
+  const client = redis.getClient();
+  const currentCapacity = meterReading.whGenerated - meterReading.whUsed;
+
+  await client.zaddAsync([
+    keyGenerator.getCapacityRankingKey(),
+    currentCapacity,
+    meterReading.siteId,
+  ]);
+};
 
 const getReport = limit => 'Redis TODO';
 
