4fcda34ecda9ef4a34b754941a9a9260cbf961c8
Simon Prickett <simon@redislabs.com>
2019-07-11 22:26:03 -0700

    Implemented updateBasic method.

--- ru102js/redisolar/src/daos/impl/redis/sitestats_dao_redis_impl.js
+++ ru102js/redisolar/src/daos/impl/redis/sitestats_dao_redis_impl.js
@@ -1,6 +1,47 @@
+const redis = require('./redis_client');
+const keyGenerator = require('./redis_key_generator');
+const timeUtils = require('../../../utils/time_utils');
+
+const weekSeconds = 60 * 60 * 24 * 7;
+
 const findById = (siteId, timestamp) => 'Redis TODO';
 
-const update = meterReading => 'Redis TODO';
+const updateBasic = async (key, meterReading) => {
+  const client = redis.getClient();
+
+  await client.hsetAsync(key, 'lastReportingTime', timeUtils.getCurrentTimestamp());
+  await client.hincrbyAsync(key, 'meterReadingCount', 1);
+  await client.expireAsync(key, weekSeconds);
+
+  const maxWh = await client.hgetAsync(key, 'maxWhGenerated');
+  if (maxWh === null || meterReading.whGenerated > parseFloat(maxWh, 10)) {
+    await client.hset(key, 'maxWhGenerated', meterReading.whGenerated);
+  }
+
+  const minWh = await client.hgetAsync(key, 'minWhGenerated');
+  if (minWh === null || meterReading.whGenerated < parseFloat(minWh, 10)) {
+    await client.hset(key, 'minWhGenerated', meterReading.whGenerated);
+  }
+
+  const maxCapacity = await client.hgetAsync(key, 'maxCapacity');
+  const thisReadingCapacity = meterReading.whGenerated - meterReading.whUsed;
+  if (maxCapacity === null || thisReadingCapacity > parseFloat(maxCapacity, 10)) {
+    await client.hset(key, 'maxCapacity', thisReadingCapacity);
+  }
+};
+
+const updateOptimized = async (key, meterReading) => {
+  const client = redis.getClient();
+
+  // TODO implementation...
+};
+
+const update = async (meterReading) => {
+  const key = keyGenerator.getSiteStatsKey(meterReading.siteId, meterReading.dateTime);
+
+  await updateBasic(key, meterReading);
+  // await updateOptimized(key, meterReading);
+};
 
 module.exports = {
   findById,
