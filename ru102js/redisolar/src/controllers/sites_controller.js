8bc8324ab1c0837355326efa1bfe50bad209c898
Simon Prickett <simon@redislabs.com>
2019-07-15 12:01:04 -0700

    Cleanup for findByGeo methods.

--- ru102js/redisolar/src/controllers/sites_controller.js
+++ ru102js/redisolar/src/controllers/sites_controller.js
@@ -7,19 +7,17 @@ const getSites = async () => siteDao.findAll();
 const getSite = async siteId => siteDao.findById(siteId);
 
 const getSitesNearby = async (lat, lng, radius, radiusUnit, onlyExcessCapacity) => {
-  console.log(`lat: ${lat}`);
-  console.log(`lng: ${lng}`);
-  console.log(`radius: ${radius}`);
-  console.log(`radiusUnit: ${radiusUnit}`);
-  console.log(`onlyExcessCapacity: ${onlyExcessCapacity}`);
-
-  return siteDao.findByGeo(
+  const matchingSites = await siteDao.findByGeo(
     lat,
     lng,
     radius,
     radiusUnit,
     onlyExcessCapacity,
   );
+
+  console.log(`<<<< CONTROLLER HAS ${matchingSites.length} SITES <<<<`);
+
+  return matchingSites;
 };
 
 module.exports = {
