746c97596678ec79cbb178b3746f35c816d466de
Simon Prickett <simon@redislabs.com>
2019-07-22 10:04:29 -0700

    Adds key schema and redis type comments.

--- ru102js/redisolar/src/daos/impl/redis/redis_key_generator.js
+++ ru102js/redisolar/src/daos/impl/redis/redis_key_generator.js
@@ -1,14 +1,21 @@
 const config = require('better-config');
 const timeUtils = require('../../../utils/time_utils');
 
+// Prefix that all keys will start with, taken from config.json
 let prefix = config.get('dataStores.redis.keyPrefix');
 
 const getKey = key => `${prefix}:${key}`;
 
+// sites:info:[siteId]
+// Redis type: hash
 const getSiteHashKey = siteId => getKey(`sites:info:${siteId}`);
 
+// sites:ids
+// Redis type: set
 const getSiteIDsKey = () => getKey('sites:ids');
 
+// sites:stats:[year-month-day]:[siteId]
+// Redis type: sorted set
 const getSiteStatsKey = (siteId, timestamp) => getKey(`sites:stats:${timeUtils.getDateString(timestamp)}:${siteId}`);
 
 const getRateLimiterKey = (name, interval, maxHits) => {
@@ -16,18 +23,30 @@ const getRateLimiterKey = (name, interval, maxHits) => {
   return getKey(`limiter:${name}:${Math.floor(minuteOfDay / interval)}:${maxHits}`);
 };
 
+// sites:geo
+// Redis type: geo
 const getSiteGeoKey = () => getKey('sites:geo');
 
+// sites:capacity:ranking
+// Redis type: sorted set
 const getCapacityRankingKey = () => getKey('sites:capacity:ranking');
 
+// sites:ts:[siteId]:[unit]
+// Redis type: RedisTimeSeries
 const getTSKey = (siteId, unit) => getKey(`sites:ts:${siteId}:${unit}`);
 
+// metric:[unit][year-month-day]:[siteId]
+// Redis type: sorted set
 const getDayMetricKey = (siteId, unit, timestamp) => getKey(
   `metric:${unit}:${timeUtils.getDateString(timestamp)}:${siteId}`,
 );
 
+// sites:feed
+// Redis type: stream
 const getGlobalFeedKey = () => getKey('sites:feed');
 
+// sites:feed:[siteId]
+// Redis type: stream
 const getFeedKey = siteId => getKey(`sites:feed:${siteId}`);
 
 const setPrefix = (newPrefix) => {
