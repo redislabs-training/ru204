83543bbabde679d6e173e6ac0af83ca43740af14
Simon Prickett <simon@redislabs.com>
2019-07-29 12:09:45 -0700

    JSDoc improvements.

--- ru102js/redisolar/src/app.js
+++ ru102js/redisolar/src/app.js
@@ -1,5 +1,6 @@
 const config = require('better-config');
 
+// Load the configuration file.
 config.set('../config.json');
 
 const express = require('express');
@@ -13,14 +14,20 @@ const banner = require('./utils/banner');
 
 const app = express();
 
+// Set up Express components.
 app.use(morgan('combined', { stream: logger.stream }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// Serve the Vue files statically from the 'public' folder.
 app.use(express.static(path.join(__dirname, '../public')));
+
+// Serve dynamic API routes with '/api/' path prefix.
 app.use('/api', routes);
 
 const port = config.get('application.port');
 
+// Start the server.
 app.listen(port, () => {
   banner();
   logger.info(`RediSolar listening on port ${port}, using database: ${config.get('application.dataStore')}`);
