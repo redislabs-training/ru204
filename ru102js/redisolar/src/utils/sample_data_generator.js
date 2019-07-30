fe77595be79bff42765551a1bce9d45f5db7288e
Simon Prickett <simon@redislabs.com>
2019-07-30 10:32:08 -0700

    JSDoc additions.

--- ru102js/redisolar/src/utils/sample_data_generator.js
+++ ru102js/redisolar/src/utils/sample_data_generator.js
@@ -2,14 +2,40 @@ const moment = require('moment');
 
 const meterReadingsController = require('../controllers/meterreadings_controller');
 
+// Max value we allow temperatures to reach when generating sample data.
 const maxTempC = 30;
 
+/**
+ * Calculates the max possible watt hours generated in a minute, for a
+ * site with the specified capacity.
+ *
+ * @param {number} capacity - the site capacity.
+ * @returns {number} - the max possible watt hours generated this site can
+ *  achieve in one minute.
+ * @private
+ */
 const getMaxMinuteWHGenerated = capacity => capacity * 1000 / 24 / 60;
 
+/**
+ * Get the initial watt hours used figure for the first minute,
+ * based on a site's given maximum capacity.
+ *
+ * @param {number} maxCapacity - the maximum capacity of the site.
+ * @returns {number} - the initial minute watt hour used figure to use.
+ * @private
+ */
 const getInitialMinuteWHUsed = maxCapacity => (
   Math.random() > 0.5 ? maxCapacity + 0.1 : maxCapacity - 0.1
 );
 
+/**
+ * Gets the next value in a series of values.
+ *
+ * @param {*} current - the current value.
+ * @param {*} max - the maximum allowed value.
+ * @returns {number} - the next value.
+ * @private
+ */
 const getNextValueInSeries = (current, max) => {
   const stepSize = 0.1 * max;
 
@@ -24,8 +50,22 @@ const getNextValueInSeries = (current, max) => {
   return current - stepSize;
 };
 
+/**
+ * Gets the next value in a series of values.
+ *
+ * @param {number} max - the maximum allowed value.
+ * @returns {number} - the next value.
+ * @private
+ */
 const getNextValue = max => getNextValueInSeries(max, max);
 
+/**
+ * Generates historical sample data for each site in the 'sites' array.
+ *
+ * @param {Array} sites - array of site objects.
+ * @param {number} days - how many days to generate data for (1-365 inclusive).
+ * @returns {Promise} - a promise that resolves when the data has been generated.
+ */
 const generateHistorical = async (sites, days) => {
   if (days < 1 || days > 365) {
     throw new Error(`Historical data generation requests must be for 1-365 days, not ${days}.`);
