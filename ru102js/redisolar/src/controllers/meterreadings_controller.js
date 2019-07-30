bb26936bb3cf200409ea65283bea48857170eacb
Simon Prickett <simon@redislabs.com>
2019-07-30 09:41:33 -0700

    JSDoc improvements.

--- ru102js/redisolar/src/controllers/meterreadings_controller.js
+++ ru102js/redisolar/src/controllers/meterreadings_controller.js
@@ -3,6 +3,14 @@ const siteStatsDao = require('../daos/sitestats_dao');
 const capacityDao = require('../daos/capacity_dao');
 const feedDao = require('../daos/feed_dao');
 
+/**
+ * Receives an array of meter reading objects and updates the
+ * database for these.  Writes to metrics, site stats, capacity
+ * and feed DAOs.
+ *
+ * @param {Array} meterReadings - array of meterreading objects.
+ * @returns {Promise} - a promise that resolves when the operation is complete.
+ */
 const createMeterReadings = async (meterReadings) => {
   for (const meterReading of meterReadings) {
     /* eslint-disable no-await-in-loop */
@@ -14,8 +22,23 @@ const createMeterReadings = async (meterReadings) => {
   }
 };
 
+/**
+ * Retrieve entries from the global meter reading feed, up to the number
+ * of entries specified by 'limit'.
+ *
+ * @param {number} limit - the maximum number of entries to retrieve from the feed.
+ * @returns {Promise} - promise that resolves to an array of feed entries.
+ */
 const getMeterReadings = async limit => feedDao.getRecentGlobal(limit);
 
+/**
+ * Retrieve entries from an individual site's meter reading feed, up to the
+ * number of entries specified by 'limit'.
+ *
+ * @param {number} siteId - the numeric ID of the site to retrieve entries for.
+ * @param {number} limit - the maximum number of entries to retrieve from the feed.
+ * @returns {Promise} - promise that resolves to an array of feed entries.
+ */
 const getMeterReadingsForSite = async (siteId, limit) => feedDao.getRecentForSite(siteId, limit);
 
 module.exports = {
