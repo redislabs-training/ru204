0aa1a4bebb4329ea6e0848973c0f75fa43047df9
Simon Prickett <simon@redislabs.com>
2019-08-07 09:31:15 -0700

    Marked tests for challenge 1 as initially skipped.

--- ru102js/redisolar/tests/site_dao_redis_impl.test.js
+++ ru102js/redisolar/tests/site_dao_redis_impl.test.js
@@ -147,7 +147,8 @@ test(`${testSuiteName}: findById with missing site`, async () => {
   expect(site).toBe(null);
 });
 
-test(`${testSuiteName}: findAll with multiple sites`, async () => {
+// This test is for Challenge #1.
+test.skip(`${testSuiteName}: findAll with multiple sites`, async () => {
   const sites = [{
     id: 1,
     capacity: 4.5,
@@ -199,7 +200,8 @@ test(`${testSuiteName}: findAll with multiple sites`, async () => {
   expect(sitesFromRedis).toEqual(expect.arrayContaining(sites));
 });
 
-test(`${testSuiteName}: findAll with empty sites`, async () => {
+// This test is for Challenge #1.
+test.skip(`${testSuiteName}: findAll with empty sites`, async () => {
   const sites = await redisSiteDAO.findAll();
   expect(sites).toEqual([]);
 });
