ad9f45a0be4e196c4470d76ffddff383e257dfee
Alvin Richards <alvin.richards@mac.com>
2019-08-13 16:56:16 -0700

    Updated script

--- analytics/grade_loader.sh
+++ analytics/grade_loader.sh
@@ -6,11 +6,18 @@ function ctrl_c() {
         exit
 }
 
+echo "START Loading"
+echo `date`
 
 for f in $2/*.csv
 do
   COURSE=$(echo $f| cut -d'_' -f 2)
   RUN=$(echo $f| cut -d'_' -f 3,4)
   echo "Processing $COURSE $RUN"
+  echo "python3 /src/grade_loader.py $1 $COURSE $RUN $f"
   python3 /src/grade_loader.py $1 $COURSE $RUN $f
+  echo ""
 done
+
+echo `date`
+echo "END Loading"
