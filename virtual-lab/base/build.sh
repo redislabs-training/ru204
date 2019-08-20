e736d64b1018ab6ced7e40c37d0a98755faf8cea
Alvin Richards <alvin.richards@mac.com>
2019-08-20 15:46:54 -0700

    Added courses.sql to create a view of the courses that have been run

--- virtual-lab/base/build.sh
+++ virtual-lab/base/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export _ALPINE_VER="3.7"
 export _REDIS_VER="5.0.3"
 export _REDIS_SHA="e290b4ddf817b26254a74d5d564095b11f9cd20d8f165459efa53eb63cd93e02"
 export _REDIS_URL="http://download.redis.io/releases/redis-$_REDIS_VER.tar.gz"
@@ -19,6 +20,10 @@ then
  	export _REDIS_URL=$3
 fi
 
-echo Building $_REDIS_VER
+echo Building $_REDIS_VER on $_ALPINE_VER
 
-docker build -t redisuniversity/virtual-lab-base:$_REDIS_VER $_BUILD_ARGS --build-arg REDIS_VERSION=$_REDIS_VER --build-arg REDIS_DOWNLOAD_SHA=$_REDIS_SHA --build-arg REDIS_DOWNLOAD_URL=$_REDIS_URL .
+docker build -t redisuniversity/virtual-lab-base:$_REDIS_VER $_BUILD_ARGS \
+  --build-arg ALPINE_VERSION=$_ALPINE_VER \
+  --build-arg REDIS_VERSION=$_REDIS_VER \
+  --build-arg REDIS_DOWNLOAD_SHA=$_REDIS_SHA \
+  --build-arg REDIS_DOWNLOAD_URL=$_REDIS_URL .
