bf549d940d3c4302ad9708d51261ecc4361371a2
Simon Prickett <simon@redislabs.com>
2019-09-05 12:30:37 -0700

    Improved JS doc for rate limiter.

--- ru102js/redisolar/src/daos/impl/redis/ratelimiter_dao_redis_impl.js
+++ ru102js/redisolar/src/daos/impl/redis/ratelimiter_dao_redis_impl.js
@@ -2,15 +2,20 @@ const redis = require('./redis_client');
 const keyGenerator = require('./redis_key_generator');
 
 /**
- * Record a hit against a unique resource that is being
- * rate limited.  Will return 0 when the resource has hit
- * the rate limit.  Fixed time window implementation.
- *
- * @param {string} name - the unique name of the resource.
- * @param {Object} opts - object containing maxHits and interval details.
- * @returns {Promise} - Promise resolving to the number of hits remaining.
- * @private
- */
+  * Record a hit against a unique resource that is being
+  * rate limited.  Will return 0 when the resource has hit
+  * the rate limit.
+  * @param {string} name - the unique name of the resource.
+  * @param {Object} opts - object containing interval and maxHits details:
+  *   {
+  *     interval: 1,
+  *     maxHits: 5
+  *   }
+  * @returns {Promise} - Promise that resolves to number of hits remaining,
+  *   or 0 if the rate limit has been exceeded..
+  *
+  * @private
+  */
 const hitFixedWindow = async (name, opts) => {
   const client = redis.getClient();
   const key = keyGenerator.getRateLimiterKey(name, opts.interval, opts.maxHits);
@@ -51,8 +56,13 @@ module.exports = {
    * rate limited.  Will return 0 when the resource has hit
    * the rate limit.
    * @param {string} name - the unique name of the resource.
-   * @param {Object} opts - object containing maxHits and interval details.
-   * @returns {Promise} - Promise that resolves to number of hits remaining.
+   * @param {Object} opts - object containing interval and maxHits details:
+   *   {
+   *     interval: 1,
+   *     maxHits: 5
+   *   }
+   * @returns {Promise} - Promise that resolves to number of hits remaining,
+   *   or 0 if the rate limit has been exceeded..
    */
   hit: hitFixedWindow,
 };
--- ru102js/redisolar/src/daos/ratelimiter_dao.js
+++ ru102js/redisolar/src/daos/ratelimiter_dao.js
@@ -8,8 +8,13 @@ module.exports = {
    * rate limited.  Will return 0 when the resource has hit
    * the rate limit.
    * @param {string} name - the unique name of the resource.
-   * @param {Object} opts - object containing maxHits and interval details.
-   * @returns {Promise} - Promise that resolves to number of hits remaining.
+   * @param {Object} opts - object containing interval and maxHits details:
+   *   {
+   *     interval: 1,
+   *     maxHits: 5
+   *   }
+   * @returns {Promise} - Promise that resolves to number of hits remaining,
+   *   or 0 if the rate limit has been exceeded..
    */
   hit: async (name, opts) => impl.hit(name, opts),
 };
