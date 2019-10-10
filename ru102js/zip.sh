3a1419bcd6de69a7137bad6f655456375eaf277c
Simon Prickett <simon@redislabs.com>
2019-10-10 11:07:40 -0700

    Updated to use project not redisolar.

--- ru102js/zip.sh
+++ ru102js/zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f ru102js-project.zip
-zip -r ru102js-project.zip redisolar -x "redisolar/.vscode/*" -x "redisolar/node_modules/*" -x redisolar/.gitignore -x "redisolar/**/.DS_Store"
+zip -r ru102js-project.zip project -x "project/.vscode/*" -x "project/node_modules/*" -x project/.gitignore -x "project/**/.DS_Store"
