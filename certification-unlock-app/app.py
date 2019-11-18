cfeb1fa5a8da9947b385efd298464a97f102674d
Simon Prickett <simon@redislabs.com>
2019-11-18 09:57:13 -0800

    Added session handling basics.

--- certification-unlock-app/app.py
+++ certification-unlock-app/app.py
@@ -1,22 +1,25 @@
 from flask import Flask
-from flask import request
 from flask import redirect
 from flask import render_template
+from flask import request
+from flask import session
+import secrets
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
 
 @app.route('/')
 def home():
-    return redirect("/login", code=302)
+    return redirect('/login', code=302)
 
-# TODO ESTABLISH A SESSION HERE
-# TODO PASSWORD FROM ENVIRONMENT
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if (request.method == 'GET'):
         return render_template('login.html')
     else:
+        # TODO PASSWORD FROM ENVIRONMENT
         if (request.form['password'] == 'secret'):
+            #session['authenticated'] = True
             return render_template('email.html')
         else:
             return render_template('login.html', error='Invalid password.')
@@ -24,4 +27,13 @@ def login():
 # TODO NEED TO HAVE A SESSION TO CHECK HERE...
 @app.route('/student', methods=['POST'])
 def enrollStudent():
-    return "TODO: Enroll student!"
+    try:
+        if (session['authenticated']):
+            return "TODO: Enroll student!"
+            # TODO END THE SESSION WHEN THEY ARE SUCCESSFULLY REGISTERED
+        else:
+            # No session, go to login...
+            return redirect('/login', code = 302)
+    except KeyError:
+        # No session, go to login...
+        return redirect('/login', code = 302)
