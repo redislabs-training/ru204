e82f3adc296d920ca4ccca35fbe8fca98a9f154b
Simon Prickett <simon@redislabs.com>
2019-11-19 12:14:36 -0800

    Secret comes from env.

--- certification-unlock-app/app.py
+++ certification-unlock-app/app.py
@@ -3,10 +3,19 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import session
+
+import os
 import secrets
+import sys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
+
+try:
+    loginPassword = os.environ['CERTIFICATION_UNLOCK_PASSWORD']
+except KeyError:
+    print("Missing required environment variabe: CERTIFICATION_UNLOCK_PASSWORD")
+    sys.exit(1)
 
 @app.route('/')
 def home():
@@ -19,8 +28,7 @@ def login():
         session.clear()
         return render_template('login.html')
     else:
-        # TODO PASSWORD FROM ENVIRONMENT
-        if (request.form['password'] == 'secret'):
+        if (request.form['password'] == loginPassword):
             session['authenticated'] = True
             return render_template('email.html')
         else:
@@ -31,8 +39,10 @@ def login():
 def enrollStudent():
     try:
         if (session['authenticated']):
+            print(request.form['email'])
             return "TODO: Enroll student!"
             # TODO END THE SESSION WHEN THEY ARE SUCCESSFULLY REGISTERED
+            # TODO REDIRECT TO THE COURSE URL WHEN THEY ARE SUCCESSFULLY REGISTERED
             # session.clear()
         else:
             # No session, go to login...
