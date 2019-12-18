076c84183bd685ac31abcddd07481dd84d27ad03
Simon Prickett <simon@redislabs.com>
2019-12-17 17:09:15 -0800

    Adds example showing how to route via cookie.

--- cloudflare-workers/redisu-router/index.js
+++ cloudflare-workers/redisu-router/index.js
@@ -18,22 +18,27 @@ async function handleRequest(request) {
     // on whether the user is logged in or not...
     console.log('HEADERS:')
     console.log(request.headers)
+    
+    const cookies = request.headers.get('Cookie')
 
     r.delete('.*/assets/.*', req => fetch(replaceDomain(req.url)))
-    r.delete('/', req => fetch(replaceDomain(req.url)))
     r.get('.*/assets/.*', req => fetch(replaceDomain(req.url)))
-    r.get('/', req => fetch(replaceDomain(req.url)))
     r.head('.*/assets/.*', req => fetch(replaceDomain(req.url)))
-    r.head('/', req => fetch(replaceDomain(req.url)))
     r.options('.*/assets/.*', req => fetch(replaceDomain(req.url)))
-    r.options('/', req => fetch(replaceDomain(req.url)))
     r.patch('.*/assets/.*', req => fetch(replaceDomain(req.url)))
-    r.patch('/', req => fetch(replaceDomain(req.url)))
     r.post('.*/assets/.*', req => fetch(replaceDomain(req.url)))
     r.post('/', req => fetch(replaceDomain(req.url)))
     r.put('.*/assets/.*', req => fetch(replaceDomain(req.url)))
-    r.put('/', req => fetch(replaceDomain(req.url)))
 
+    // If not logged in, override homepage else send to origin.
+    if (cookies && cookies.indexOf('_ga=GA1.2.2027235534.1575393409') == -1) {
+        r.delete('/', req => fetch(replaceDomain(req.url)))
+        r.get('/', req => fetch(replaceDomain(req.url)))
+        r.head('/', req => fetch(replaceDomain(req.url)))
+        r.options('/', req => fetch(replaceDomain(req.url)))
+        r.patch('/', req => fetch(replaceDomain(req.url)))
+        r.put('/', req => fetch(replaceDomain(req.url)))        
+    }
 
     // Send everything else to the original origin...
     r.delete('.*', req => fetch(req))
