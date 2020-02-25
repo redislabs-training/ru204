af69d01e10002386d5b6de3202428ae1220f6f60
Simon Prickett <simon@redislabs.com>
2020-02-25 15:54:51 -0800

    Test suite for sliding window rate limiter 102JS.

--- ru102js/project/tests/sliding_ratelimiter_dao_redis_impl.test.js
+++ ru102js/project/tests/sliding_ratelimiter_dao_redis_impl.test.js
@@ -30,28 +30,69 @@ afterAll(() => {
   client.quit();
 });
 
-// Challenge 7. TODO THIS SHOULD BE SKIPPED INITIALLY
-test(`${testSuiteName}: hit (sliding window limit not exceeded)`, async () => {
-  const limiterOpts = {
+const sleep = (milliseconds) => {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+};
+
+const runSlidingWindowTests = async (name, limiterOpts, howMany) => {
+  const results = [];
+
+  for (n = 0; n < howMany; n += 1) {
+    const remains = await redisRateLimiterDAO.hit(name, limiterOpts);
+    results.push(remains);
+  }
+
+  return results;
+};
+
+// Challenge 7. Remove '.skip' to enable test.
+test.skip(`${testSuiteName}: hit (sliding window limit not exceeded)`, async () => {
+  const results = await runSlidingWindowTests('testresource', {
     interval: 10000,
+    maxHits: 5,
+  }, 5);
+
+  expect(results).toStrictEqual([4, 3, 2, 1, 0]);
+});
+
+// Challenge 7. Remove '.skip' to enable test.
+test.skip(`${testSuiteName}: hit (sliding window limit exceeded)`, async () => {
+  let results = await runSlidingWindowTests('testresource2', {
+    interval: 10000,
+    maxHits: 5,
+  }, 6);
+
+  expect(results).toStrictEqual([4, 3, 2, 1, 0, -1]);
+
+  results = await runSlidingWindowTests('testresource3', {
+    interval: 10000,
+    maxHits: 5,
+  }, 8);
+
+  expect(results).toStrictEqual([4, 3, 2, 1, 0, -1, -1, -1]);
+});
+
+// Challenge 7. Remove '.skip' to enable test.
+test.skip(`${testSuiteName}: hit (sliding window ensure window slides)`, async () => {
+  const sliderName = 'testresource4';
+  const sliderOpts = {
+    interval: 2000,
     maxHits: 5,
   };
 
-  let remains = await redisRateLimiterDAO.hit('testresource', limiterOpts);
-  console.log(remains);
-  remains = await redisRateLimiterDAO.hit('testresource', limiterOpts);
-  console.log(remains);
-  remains = await redisRateLimiterDAO.hit('testresource', limiterOpts);
-  console.log(remains);
-  remains = await redisRateLimiterDAO.hit('testresource', limiterOpts);
-  console.log(remains);
-  remains = await redisRateLimiterDAO.hit('testresource', limiterOpts);
-  console.log(remains);
-  remains = await redisRateLimiterDAO.hit('testresource', limiterOpts);
-  console.log(remains);
-});
+  let results = await runSlidingWindowTests(sliderName, sliderOpts, 3);
+  expect(results).toStrictEqual([4, 3, 2]);
 
-// Challenge 7. TODO THIS SHOULD BE SKIPPED INITIALLY
-test.todo(`${testSuiteName}: hit (sliding window limit exceeded)`);
+  sleep(2000);
+
+  // Expect the window to have reset...
+
+  results = await runSlidingWindowTests(sliderName, sliderOpts, 7);
+  expect(results).toStrictEqual([4, 3, 2, 1, 0, -1, -1]);
+});
 
 /* eslint-enable */
