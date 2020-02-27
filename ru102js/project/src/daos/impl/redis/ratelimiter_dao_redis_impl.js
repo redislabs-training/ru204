c70ee593f6861ff6c976ddacca54439fabd5f41e
Simon Prickett <simon@redislabs.com>
2020-02-27 14:10:57 -0800

    Changed fixed window rate limiter to return -1 when limit exceeded and updated tests.

--- ru102js/project/src/daos/impl/redis/ratelimiter_dao_redis_impl.js
+++ ru102js/project/src/daos/impl/redis/ratelimiter_dao_redis_impl.js
@@ -3,7 +3,7 @@ const keyGenerator = require('./redis_key_generator');
 
 /**
   * Record a hit against a unique resource that is being
-  * rate limited.  Will return 0 when the resource has hit
+  * rate limited.  Will return -1 when the resource has hit
   * the rate limit.
   * @param {string} name - the unique name of the resource.
   * @param {Object} opts - object containing interval and maxHits details:
@@ -32,7 +32,7 @@ const hitFixedWindow = async (name, opts) => {
 
   if (hits > opts.maxHits) {
     // Too many hits.
-    hitsRemaining = 0;
+    hitsRemaining = -1;
   } else {
     // Return number of hits remaining.
     hitsRemaining = opts.maxHits - hits;
