d74c66e52b7a22f247068ef433657c57c0164d6b
Simon Prickett <simon@redislabs.com>
2020-05-07 11:04:18 -0700

    Fixed container name.

--- conferences/RedisConf2020/run.sh
+++ conferences/RedisConf2020/run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name redis1-lab-bloom -p:8888:8888 redisuniversity/redisconf-2020:latest
+docker run --rm --name redis-lab-conf2020 -p:8888:8888 redisuniversity/redisconf-2020:latest
