fc8007facabd02c7fa8cf984a9e0ab4f74b22947
Simon Prickett <simon@redislabs.com>
2020-05-12 13:01:09 -0700

    Versions we used to build redisconf v3 container.

--- conferences/RedisConf2020/run.sh
+++ conferences/RedisConf2020/run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name redis-lab-conf2020 -p:8888:8888 redisuniversity/redisconf-2020:latest
+docker run --rm --name redisuniversity-redisconf-2020 -p:8888:8888 redisuniversity/redisconf-2020:latest
