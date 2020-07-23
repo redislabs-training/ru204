d53c6567955fec84880ded7165a0fd892f6b7b7d
Simon Prickett <simon@redislabs.com>
2020-07-22 18:16:18 -0700

    Clones repo.

--- ru102py/.bashrc
+++ ru102py/.bashrc
@@ -1,5 +1,10 @@
 PS1="\W$ "
 
+if [ ! -f /src/redisu/project/README.md ]; then
+   /home/student/get_project_source.sh
+   clear
+fi
+
 if [ -f ~/.logo ]; then
    cat ~/.logo
 fi
---
+++ ru102py/get_project_source.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /src/redisu
+git clone -b master --single-branch https://github.com/redislabs-training/ru102py.git project
+git clone -b solutions --single-branch https://github.com/redislabs-training/ru102py.git solutions
+
