2044a570746868d81be8d4168f25bbcdd592db41
Simon Prickett <simon@redislabs.com>
2020-08-04 17:57:32 -0700

    Updated to get code from GitHub.

--- ru201/build.sh
+++ ru201/build.sh
@@ -12,4 +12,9 @@ fi
 sed 's/$REDIS_VERSION/'$_REDIS_VER'/' Dockerfile.template > Dockerfile.$_REDIS_VER
 echo Building $_REDIS_VER
 
+rm -rf redisu/data
+mkdir -p redisu/data
+git clone -b master --single-branch https://github.com/redislabs-training/ru201.git redisu/data
+rm -rf redisu/data/.git
+
 docker build -f Dockerfile.$_REDIS_VER -t redisuniversity/ru201-lab .
