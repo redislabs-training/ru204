28f2c051ca998107682b720be8047b904846128b
Simon Prickett <simon@redislabs.com>
2020-09-29 10:55:12 -0700

    Adds git clone for 202 project.

--- ru202/build.sh
+++ ru202/build.sh
@@ -12,4 +12,9 @@ fi
 sed 's/$REDIS_VERSION/'$_REDIS_VER'/' Dockerfile.template > Dockerfile.$_REDIS_VER
 echo Building $_REDIS_VER
 
+rm -rf src
+mkdir src
+git clone -b master --single-branch https://github.com/redislabs-training/ru202.git src
+rm -rf src/.git
+
 docker build -f Dockerfile.$_REDIS_VER -t redisuniversity/ru202-lab .
