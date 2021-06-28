fffe620b6d5ea3320bf0e9916f87ff6b7a4a74aa
Justin Castilla <pxlperfection@gmail.com>
2021-06-28 14:46:31 -0700

    add more/better error-handling

--- cert_checker_service/index.js
+++ cert_checker_service/index.js
@@ -30,15 +30,23 @@ const getUserName = async (user_id) => {
 
 /* check lms for completed courses and ensure necessary classes are completed */
 const checkEligibility = async (user_id) => {
+  let response;
   try{                 
     /* fetch all completed courses */
-    const response = await lms.query({
+    response = await lms.query({
       text:  `SELECT label, context_username
               FROM javascript.edx_certificate_created 
               WHERE user_id=$1::text`,
       values: [user_id],
       rowMode: 'array',
     })
+  } catch (e) {
+    console.error('Error Occurred Fetching Data from Redshift: ', e);
+    return {
+      success: false,
+      message: `Error from Redshift: ${e.message}`
+    }
+  }
     username = response.rows[0][1]
     /* strip course versions and flattens response */
     const rawCourses = response.rows
@@ -48,35 +56,42 @@ const checkEligibility = async (user_id) => {
     /* deduplicate completed course rows */
     const courses = [...new Set(rawCourses)]
     /* check for presence of RU101, RU202, and an 'elective' class */
-    const ru101Elig = courses.splice(courses.indexOf('RU101'), 1).length === 1
-    const ru202Elig = courses.splice(courses.indexOf('RU202'), 1).length === 1
+    const ru101Completed = courses.splice(courses.indexOf('RU101'), 1).length === 1
+    const ru202Completed = courses.splice(courses.indexOf('RU202'), 1).length === 1
     const electives = courses.flat()
     return {
-            'RU101': ru101Elig,
-            'RU202': ru202Elig,
+            'RU101': ru101Completed,
+            'RU202': ru202Completed,
             'electives': electives,
           }
-  } catch (e) {
-    console.error('Error Occurred Fetching Data from Redshift: ', e);
-    return {
-      success: false,
-      message: `Unable to find user with id: ${user_id}`
-    }
-  }
 }
 
 /* enroll student into current Exam */
 const enrollStudentWithAppsembler = async (username) => {
 
   const enrollmentEndpoint = `${config.TAHOEENDPOINT}enrollments/`
-
-  const payload = {
-      'action': 'enroll',
-      'email_learners': true,
-      'auto_enroll': true,
-      'courses': [ await getNewestExamCourse() ],
-      'identifiers': [ username ]
+  let newestExamCourse
+  try {
+    newestExamCourse = await getNewestExamCourse()
+  } catch(e) {
+    console.error('Enroll student with newest course error: ', e.mesage)
+    return {
+      success: false,
+      message: e.message
     }
+  }
+
+  /* Payload: 
+    'action': we are enrolling the user in a course
+    'email_learners': sends an email to user when enrolled
+    'courses': we pull the newest Exam course from our Appsembler instance
+    'identifiers': username or email-address of the user */
+  const payload = {
+    'action': 'enroll',
+    'email_learners': true,
+    'courses': [ await getNewestExamCourse() ],
+    'identifiers': [ username ]
+  }
   const headers = {
     'Authorization': config.APPSEMBLER_TOKEN,
     'Content-Type': 'application/json',
@@ -90,7 +105,7 @@ const enrollStudentWithAppsembler = async (username) => {
       { headers: headers })
     return {success: true}
   } catch(e) {
-    console.error('Error: ', e)
+    console.error('Enrollment Error: ', e.mesage)
     return {
       success: false,
       message: e.message
@@ -100,9 +115,17 @@ const enrollStudentWithAppsembler = async (username) => {
 
 /* polls Appsembler for most recent Exam course based on version number */
 const getNewestExamCourse = async () => {
-  const newestExamCourse = await axios.get(`${config.TAHOEENDPOINT}courses/?number=DEVELOPER-CERTIFICATION-EXAM-1`, { headers: {
-    Authorization: config.APPSEMBLER_TOKEN,
-  }})
+  let newestExamCourse
+  try {
+    newestExamCourse= await axios.get(`${config.TAHOEENDPOINT}courses/?number=DEVELOPER-CERTIFICATION-EXAM-1`, { 
+      headers: { Authorization: config.APPSEMBLER_TOKEN }
+    })
+  } catch(e) {
+    console.error('getNewestExamCourse Error: ', e.mesage)
+  }
+  if(newestExamCourse.data.results.length === 0){
+    throw new Error('No developer certification exam courses found')
+  }
   const course_id = newestExamCourse
                       .data
                       .results[newestExamCourse.data.results.length-2]
@@ -113,7 +136,7 @@ const getNewestExamCourse = async () => {
 app.options('*', cors(cors_options))
 
 app.get('/check-availability', cors(cors_options), async (req, res) => {
-  const { user_id } = req.query
+  const user_id = req.query.user_id
   if(!user_id || user_id === 'undefined') {
     res.json({ error: 'user_id is undefined or unavailable', user_id: user_id })
   }
