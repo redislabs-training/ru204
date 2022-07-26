5aac95139e4f01d2393f1d5e6ad009062d6ba5ba
Simon Prickett <simon@redislabs.com>
2022-07-26 21:40:03 +0100

    Adds beginnings of Java example.

---
+++ src/java/jedis_example/src/test/java/com/redis/university/ru204/AppTest.java
@@ -0,0 +1,14 @@
+package com.redis.university.ru204;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
