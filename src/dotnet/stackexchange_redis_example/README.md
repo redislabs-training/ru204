58213ded6a0a3d0871c1efaf4e99488c2be91d5d
slorello89 <42971704+slorello89@users.noreply.github.com>
2022-07-27 09:39:20 -0400

    adding .NET example

---
+++ src/dotnet/stackexchange_redis_example/README.md
@@ -0,0 +1,19 @@
+# RU204 StackExchange.Redis Example
+
+The following example demonstrates the execution of RedisJSON commands using the [StackExchange.Redis Client](https://github.com/stackexchange/StackExchange.Redis).
+
+## Prerequisites
+
+* You'll need the [.NET 6 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/6.0) or later installed
+* You'll probably want to have an IDE for writing C# code, e.g. [Visual Studio](https://visualstudio.microsoft.com/), [JetBrains Rider](https://www.jetbrains.com/rider/), or [VS Code](https://code.visualstudio.com/)
+* You will need an instance of Redis Stack running, this can be done locally with docker `docker run -d -p 6379:6379 -p 8001:8001 redis/redis-stack`, or by following one of the other [Redis Stack instillation guides](https://redis.io/docs/stack/get-started/install/), or of course via [Redis Cloud](https://app.redislabs.com/#/)
+* If you are not running Redis Stack locally on port 6379, you will need to set the `REDIS_CONNECTION_STRING` environment variable to a valid StackExchange.Redis [connection string](https://stackexchange.github.io/StackExchange.Redis/Configuration#basic-configuration-strings)
+
+## How to Run the App
+
+After the prerequisites are met, all you need to do to run the app is to run the following command in your terminal from this directory `dotnet run`.
+
+## Where is the Code?
+
+The code for this example is located in StackExchange.Redis.Example/Program.cs
+
---
+++ src/dotnet/stackexchange_redis_example/StackExchange.Redis.Example/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+const string BOOK_KEY = "ru204:book:3";
+
+var bookJson = await File.ReadAllTextAsync("data/Book.json");
+
+
+var connectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING") ?? "localhost";
+
+Console.WriteLine($"Connecting to redis at {connectionString}");
+var muxer = ConnectionMultiplexer.Connect(connectionString);
+var db = muxer.GetDatabase();
+
+// Delete any previous data at our book's key
+await db.KeyDeleteAsync(BOOK_KEY);
+
+// Store the book in Redis at the key ru204:Book:3
+// Response Should be "OK"
+var response = await db.ExecuteAsync("JSON.SET", BOOK_KEY, "$", bookJson);
+Console.WriteLine($"Book stored: {response}");
+
+// let's get the the author and score for this book
+// Response will be:  {"$.metrics.score":[2.3],"$.author":["Redis University"]}
+response = await db.ExecuteAsync("JSON.GET", BOOK_KEY, "$.author", "$.metrics.score");
+Console.WriteLine($"Author and Score: {response}");
+
+// add one to the number of rating_votes
+// Response will be [13]
+response = await db.ExecuteAsync("JSON.NUMINCRBY", BOOK_KEY, $"$.metrics.rating_votes", 1);
+Console.WriteLine($"rating_votes incremented to {response}");
+
+// add another copy of the book to the inventory
+// response will be: 3 (new size of the inventory array)
+response = await db.ExecuteAsync("JSON.ARRAPPEND", BOOK_KEY, "$.inventory", "{\"status\":\"available\",\"stock_id\":\"3_3\"}");
+var inventoryCount = ((RedisResult[]?) response)?.FirstOrDefault();
+Console.WriteLine($"There are now {inventoryCount} copies of hte book in the inventory.");
