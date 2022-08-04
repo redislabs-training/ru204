a916b871cba4d31d105cd91611e346a36f037ee2
slorello89 <42971704+slorello89@users.noreply.github.com>
2022-08-04 08:59:46 -0400

    adding redis om .net example

---
+++ src/dotnet/Redis.OM.Example/Book.cs
@@ -0,0 +1,20 @@
+﻿using Redis.OM.Modeling;
+
+namespace Redis.OM.Example;
+
+[Document(StorageType = StorageType.Json, Prefixes = new []{"ru204:redis-om-dotnet:Book"})]
+public class Book
+{
+    [RedisIdField]
+    public Ulid Pk { get; set; }
+    public string? Id { get; set; }
+    public string? Author { get; set; }
+    public string? Description { get; set; }
+    public List<string>? Genres { get; set; }
+    public List<InventoryItem>? Inventory { get; set; }
+    public Metrics? Metrics { get; set; }
+    public uint Pages { get; set; }
+    public string? Title { get; set; }
+    public string? Url { get; set; }
+    public uint YearPublished { get; set; }
+}
