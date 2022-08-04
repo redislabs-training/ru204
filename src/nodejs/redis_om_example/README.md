9e2f816e7febdfa68b958f6431a78177a44dfcd5
Simon Prickett <simon@redislabs.com>
2022-08-04 21:23:42 +0100

    Initial Node.js OM example.

---
+++ src/nodejs/redis_om_example/README.md
@@ -0,0 +1,33 @@
+# RU204 Redis OM Node Example
+
+The following example demonstrates data modeling with RedisJSON and the [Redis OM Node.js](https://github.com/redis/redis-om-node) client.
+
+## Prerequisites
+
+* You'll need [Node.js](https://nodejs.org/) 14.8 or higher installed.
+* You will need an instance of Redis Stack.  See the [setup instructions](/README.md) in the README at the root of this repo.
+* If you are running your Redis Stack instance in the cloud or somewhere that isn't localhost:6379, you'll need to set the `REDIS_OM_URL` environment variable to point at your instance before running the code.  If you need help with the format for this, check out the [Redis URI scheme specification](https://www.iana.org/assignments/uri-schemes/prov/redis).
+
+## Setup
+
+Install Redis OM and any other dependencies before running the code:
+
+```bash
+npm install
+```
+
+Ensure that your Redis Stack instance is running, and that you have set the `REDIS_OM_URL` environment variable if necessary.  Example:
+
+```bash
+export REDIS_OM_URL=redis://user:password@host:port
+```
+
+## Run the Code
+
+```bash
+npm start
+```
+
+## View the Code
+
+The code is contained in [`json_om_example.js`](./json_om_example.js).
---
+++ src/nodejs/redis_om_example/json_om_example.js
@@ -0,0 +1,61 @@
+import { Client, Entity, Schema } from 'redis-om';
+
+const client = new Client();
+await client.open(process.env.REDIS_OM_URL || 'redis://localhost:6379');
+
+class Book extends Entity {};
+
+const bookSchema = new Schema(Book, {
+  author: { type: 'string' },
+  id: { type: 'string' }, 
+  description: { type: 'string' },
+  genres:  { type: 'string[]' },
+  pages: { type: 'number' },
+  title: { type: 'string' },
+  url: { type: 'string' },
+  yearPublished:  { type: 'number' },
+  // Redis OM Node does not yet support embedded objects, 
+  // so the metrics object has been flattened to the following
+  // two fields, and we have omitted the inventory arry of 
+  // objects for the same reason.
+  ratingVotes: { type: 'number' },
+  score: { type: 'number' }
+}, {
+  prefix: 'ru204:redis-om-node:book'
+});
+
+const bookRepository = client.fetchRepository(bookSchema);
+
+const newBook = bookRepository.createEntity({
+  author: 'Redis Staff',
+  id: '999',
+  description: 'This is a book all about Redis.',
+  genres: [ 'redis', 'tech', 'computers' ],
+  pages: 1000,
+  title: 'Redis for Beginners',
+  url: 'https://university.redis.com/courses/ru204/',
+  yearPublished: 2022,
+  ratingVotes: 4000,
+  score: 4.5
+});
+
+// Get the locally generated ULID for this book.
+console.log(`newBook ULID: ${newBook.entityId}`);
+
+// Save the book to Redis.
+await bookRepository.save(newBook);
+console.log('Saved book in Redis.');
+
+// Retrieve the book from Redis.
+const aBook = await bookRepository.fetch(newBook.entityId);
+console.log('Retrieved from Redis:');
+console.log(aBook.toJSON());
+
+// Update the author field and save it.
+aBook.author = 'Redis University';
+await bookRepository.save(aBook);
+console.log('Updated author and saved to Redis:');
+const updatedBook = await bookRepository.fetch(aBook.entityId);
+console.log(updatedBook.toJSON());
+
+await client.close();
