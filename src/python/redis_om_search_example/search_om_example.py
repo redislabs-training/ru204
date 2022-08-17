4c29387ad39c2ef22d4c4e646720cdc0de63de67
Simon Prickett <simon@redislabs.com>
2022-08-17 18:18:12 +0100

    Work in progress.

--- src/python/redis_om_search_example/search_om_example.py
+++ src/python/redis_om_search_example/search_om_example.py
@@ -1,38 +1,11 @@
-from redis_om import (JsonModel, EmbeddedJsonModel, Migrator)
-from pydantic import (PositiveInt, PositiveFloat, AnyHttpUrl)
-from typing import List
+from model import Book
 
-# This class models the embedded "metrics" object.
-class Metrics(EmbeddedJsonModel):
-    rating_votes: PositiveInt
-    score: PositiveFloat
+# Search for books written by Stephen King...
+result_set = Book.find(
+    Book.author == "Stephen King"
+).all()
 
-# This class models the "inventory" array of objects.
-class InventoryItem(EmbeddedJsonModel):
-    status: str
-    stock_id: str
+print(result_set)
 
-# This class models a book.
-class Book(JsonModel):
-    author: str
-    id: str
-    description: str
-    genres: List[str]
-    inventory: List[InventoryItem]
-    metrics: Metrics
-    pages: PositiveInt
-    title: str
-    url: AnyHttpUrl
-    year_published: PositiveInt
-
-    # Extra configuration to specify how to generate key
-    # names when saving an instance of the model in Redis.
-    class Meta:
-        global_key_prefix="ru204"
-        model_key_prefix="book"
-
-# Create the search index.
-Migrator().run()
-
-# TODO search for things...
+# TODO others
 
