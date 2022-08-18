5c6bcd8c602ce2256e628d563016579a9406e0b6
Simon Prickett <simon@redislabs.com>
2022-08-18 22:37:25 +0100

    Work in progress.

--- src/nodejs/redis_om_search_example/load_data.js
+++ src/nodejs/redis_om_search_example/load_data.js
@@ -1,1 +1,20 @@
-console.log('TODO data loader...');
+import { Client } from 'redis-om';
+import { bookSchema } from './model.js';
+import { readdir, readFile } from 'node:fs/promises';
+import { sep as pathSeparator } from 'node:path';
+
+const FILE_PATH = '../../../data/books';
+
+const client = new Client();
+await client.open(process.env.REDIS_OM_URL || 'redis://localhost:6379');
+
+const bookRepository = client.fetchRepository(bookSchema);
+
+const fileNames = await readdir(FILE_PATH);
+for (const fileName of fileNames) {
+  console.log(`${FILE_PATH}${pathSeparator}${fileName}`);
+  const bookData = JSON.parse(await readFile(`${FILE_PATH}${pathSeparator}${fileName}`));
+  console.log(bookData);
+}
+
+await client.close();
--- src/nodejs/redis_om_search_example/model.js
+++ src/nodejs/redis_om_search_example/model.js
@@ -1,1 +1,22 @@
-// TODO data model...
+import { Entity, Schema } from 'redis-om';
+
+class Book extends Entity { };
+
+export const bookSchema = new Schema(Book, {
+  author: { type: 'string' },
+  id: { type: 'string' },
+  description: { type: 'string' },
+  genres: { type: 'string[]' },
+  pages: { type: 'number' },
+  title: { type: 'string' },
+  url: { type: 'string' },
+  yearPublished: { type: 'number' },
+  // Redis OM Node does not yet support embedded objects, 
+  // so the metrics object has been flattened to the following
+  // two fields, and we have omitted the inventory array of 
+  // objects for the same reason.
+  ratingVotes: { type: 'number' },
+  score: { type: 'number' }
+}, {
+  prefix: 'ru204:redis-om-node:book'
+});
