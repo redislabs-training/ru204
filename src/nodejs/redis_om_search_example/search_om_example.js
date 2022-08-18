2cc54745ea83d0ea2b68add7f026fcd3e394cfd9
Simon Prickett <simon@redislabs.com>
2022-08-18 23:14:43 +0100

    Scaffolded the search code a little more.

--- src/nodejs/redis_om_search_example/search_om_example.js
+++ src/nodejs/redis_om_search_example/search_om_example.js
@@ -1,1 +1,11 @@
+import { Client } from 'redis-om';
+import { bookSchema } from './model.js';
+
+const client = new Client();
+await client.open(process.env.REDIS_OM_URL || 'redis://localhost:6379');
+
+const bookRepository = client.fetchRepository(bookSchema);
+
 console.log('TODO search examples...');
+
+await client.close();
