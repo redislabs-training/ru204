83592c6d4dd53631ead5ff4f9caff94565124b0c
Simon Prickett <simon@redislabs.com>
2022-08-30 14:01:38 +0100

    Made Redis location configurable.

--- src/dotnet/redis_om_search_example/Program.cs
+++ src/dotnet/redis_om_search_example/Program.cs
@@ -9,7 +9,8 @@ if (args.Length != 1 || (args[0] != "load" && args[0] != "search"))
 }
 
 var action = args[0];
-var provider = new RedisConnectionProvider("redis://localhost:6379");
+var url = Environment.GetEnvironmentVariable("REDIS_OM_URL") ?? "redis://localhost:6379";
+var provider = new RedisConnectionProvider(url);
 var books = provider.RedisCollection<Book>();
 
 if (action == "load")
